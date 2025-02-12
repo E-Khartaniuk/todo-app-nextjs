d3810bcf2d3f4cedb480d95a3758e60f2667aa85
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2025-02-12 02:29:33 +0200

    1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/todo-app-nextjs',
+  // basePath: '/todo-app-nextjs',
   // assetPrefix: '/todo-app-nextjs/',
   trailingSlash: true,  
   output: 'export',
